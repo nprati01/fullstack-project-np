923723b112b2b0a577ef87b496a33669d641a991
nprati01 <nicole.e.prati@gmail.com>
2023-07-11 20:29:35 -0400

    update content

--- intsureview_fe/src/components/FishForm.js
+++ intsureview_fe/src/components/FishForm.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function Form() {
+function FishForm() {
     return (
         <div>
             <h1>This form is for something </h1>
             <p>Display a line that tells them what the form is for </p>
-            {/* form fields will go here
-            form content must include:
-            text input
-            select input for Yes or No
-            three more inputs of my choice
-            include must validate the content of at least one input  */}
+            <p>Fish Species-textfield</p>
+            <p>Description- text field</p>
+            <p>Rarity-select input </p>
+            <p>Location seen- text field</p>
+            <p>Salt water or freshwater??</p>
+            <p>Possible fish Image</p>
             {/* Create a submit button that sends form data to backend and handle backend response */}
         </div>
 
@@ -19,4 +19,4 @@ function Form() {
 
 }
 
-export default Form;
+export default FishForm;
